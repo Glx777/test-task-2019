0cbe924a9156720da67f4948302f3131e3526b1c
dmitry.zapolskih <dmitry.zapolskih@protoware.today>
2019-07-02 12:42:44 +0400

    Change refactoring task

--- refactoringTask.js
+++ refactoringTask.js
@@ -3,21 +3,18 @@ const func = (s, a, b) => {
     if (!s) {
         return -1
     }
-    
-    var i = s.length - 1
+
     var aIndex = -1
     var bIndex = -1
-    
-    while (aIndex === -1 && bIndex === -1 && i > 0) {
-        if (s.substring(i, i + 1) === a) {
-            aIndex = i
-        }
-        
-        if (s.substring(i, i + 1) === b) {
-            bIndex = i
+
+    if (aIndex === -1 && bIndex === -1) {
+        if (s.lastIndexOf(a) > 0 && a) {
+            aIndex = s.lastIndexOf(a)
         }
 
-        i -= 1
+        if (s.lastIndexOf(b) > 0 && b) {
+            bIndex = s.lastIndexOf(b)
+        }
     }
     
     if (aIndex !== -1) {
